9f8972605544ee44972bee72485bfe730c8156f5
skylightanimation <ryunoskearima@gmail.com>
2023-12-14 23:57:46 +0700

    init

---
+++ src/main/kotlin/com/demo/apicrud/ApiCrudApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.apicrud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiCrudApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiCrudApplication>(*args)
+}
